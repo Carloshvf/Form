80371b67f6686367a91b7d2b0eca1a4ff32a0d2d
Carloshvf <cfernandhv@gmail.com>
2023-05-13 20:27:30 -0300

    Ajustes de biblioteca

---
+++ components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container d-flex justify-content-center">
+    <div class="row">
+      <div class="col">
+        <h1>Cadastre-se</h1>
+        <b-form>
+          <b-form-group label="Endereço de Email:">
+            <b-form-input
+              type="email"
+              placeholder="Email"
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group label="Nome:">
+            <b-form-input placeholder="Nome" required></b-form-input>
+          </b-form-group>
+          <b-form-group label="Senha:">
+            <b-form-input placeholder="Senha" required></b-form-input>
+          </b-form-group>
+        </b-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+};
+</script>

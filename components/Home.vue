f6424c6f63e1d1849dc3056bf3b16b1f9f50c2f3
Carloshvf <cfernandhv@gmail.com>
2023-05-14 11:37:15 -0300

    mexendo no css

--- components/Home.vue
+++ components/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container d-flex justify-content-center">
-    <div class="row">
+    <div class="form-row">
       <div class="col">
         <h1>Cadastre-se</h1>
-        <b-form>
+        <b-form class="home">
           <b-form-group label="Endereço de Email:">
             <b-form-input
               type="email"
@@ -12,12 +12,20 @@
             ></b-form-input>
           </b-form-group>
           <b-form-group label="Nome:">
-            <b-form-input placeholder="Nome" required></b-form-input>
+            <b-form-input
+              placeholder="Nome"
+              type="text"
+              required
+            ></b-form-input>
           </b-form-group>
           <b-form-group label="Senha:">
-            <b-form-input placeholder="Senha" required></b-form-input>
+            <b-form-input
+              placeholder="Senha"
+              type="password"
+              required
+            ></b-form-input>
           </b-form-group>
-          <b-button class="" @click="salvarInfo()">Salvar</b-button>
+          <b-button class="btn" @click="salvarInfo()">Salvar</b-button>
         </b-form>
       </div>
     </div>
@@ -32,7 +40,23 @@ export default {
   },
 
   methods: {
-    salvarInfo() {},
+    salvarInfo() {
+      return;
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 1em;
+}
+
+.btn-salvar {
+  text-align: center;
+  align-items: center;
+}
+</style>
---
+++ pages/cadastrados/index.vue
@@ -0,0 +1,19 @@
+<!-- <template>
+  <div class="container d-flex justify-content-center">
+    <div class="row">
+      <div class="col">
+        <h1>Informações cadastradas</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  methods: {},
+};
+</script> -->
